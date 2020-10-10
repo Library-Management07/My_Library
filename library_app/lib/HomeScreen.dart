4b600da6c573c6f19943df06f355f314e76153d1
heer <heerpatel291@gmail.com>
2020-10-10 16:59:23 +0530

    Home Page

--- library_app/lib/HomeScreen.dart
+++ library_app/lib/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:library_app/Bookstore/BookStore.dart';
 class Home_screen extends StatefulWidget {
   @override
   _Home_screenState createState() => _Home_screenState();
@@ -8,82 +9,154 @@ class Home_screen extends StatefulWidget {
 class _Home_screenState extends State<Home_screen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(50,53,94,50 ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body:
           Container(
-            alignment: Alignment.centerLeft,
-            child: FlatButton(onPressed: (){},
-                child:  Text('Account',textDirection:TextDirection.ltr ,style: TextStyle(
-                  fontSize: 30,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.white,
-                ),),
-            ),
-            margin: EdgeInsets.only(left: 70),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors:[Color.fromRGBO(129, 109, 198, 50),Color.fromRGBO(79, 103, 154, 50)],
-              begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
+            /*decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blueGrey, Colors.white70],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
-            ),
-            height: 180,width: 500,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: FlatButton(onPressed: (){},
-              child:  Text('Book Store',textDirection:TextDirection.ltr ,style: TextStyle(
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                color: Colors.white,
-              ),),
-            ), margin: EdgeInsets.only(left: 70),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors:[Color.fromRGBO(238,103 ,110 , 50),Color.fromRGBO(161,108 ,185, 50)],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
+            ),*/
+            child: Column(
+              children:[
+                Container(
+                  height: 200,
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text("My library",style: TextStyle(
+                        fontStyle: FontStyle.italic ,
+                        fontSize: 40.0,
+                      ),),
+                    ),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:AssetImage("assets/Menu_Home.png"),
+                      fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(25),
+                ),
               ),
+
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 180.0,
+                        width: 180.0,
+                        child: InkWell(
+                          onTap: (){
+                            Navigator.push(context,MaterialPageRoute(builder: (context)=>App()));
+                          },
+                          child: Container(
+                            child: Text("Book Store",style: TextStyle(fontWeight: FontWeight.bold,),),
+                            padding: EdgeInsets.only(top:70,left: 50),
+                            margin: EdgeInsets.all(10.0),
+                            //height: 170.0,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:AssetImage("assets/One.png"),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 260.0,
+                        width: 200.0,
+                        child: InkWell(
+                          onTap: (){
+                        //    Navigator.push(context,MaterialPageRoute(builder: (context)=>App()));
+                          },
+                          child: Container(
+                            child: Text("Mybook",style: TextStyle(fontWeight: FontWeight.bold,),),
+                            padding: EdgeInsets.only(top:100, left: 70),
+                            margin: EdgeInsets.all(10.0),
+                          //  height: 230.0,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:AssetImage("assets/One.png"),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 260.0,
+                        width: 200.0,
+                        child: SizedBox(
+                          child: InkWell(
+                            onTap: (){
+                              //    Navigator.push(context,MaterialPageRoute(builder: (context)=>App()));
+                            },
+                            child: Container(
+                              child: Text("Search",style: TextStyle(fontWeight: FontWeight.bold,),),
+                              padding: EdgeInsets.only(top:100, left: 70),
+                              margin: EdgeInsets.all(10.0),
+                              height: 230.0,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image:AssetImage("assets/One.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 180.0,
+                        width: 180.0,
+                        child: InkWell(
+                          onTap: (){
+                            //    Navigator.push(context,MaterialPageRoute(builder: (context)=>App()));
+                          },
+                          child: Container(
+                            child: Text("My account",style: TextStyle(fontWeight: FontWeight.bold,),),
+                            height: 170.0,
+                            padding: EdgeInsets.only(top:70,left: 50),
+                            margin: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image:AssetImage("assets/One.png"),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
             ),
-            height: 185,width: 500,
           ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: FlatButton(onPressed: (){},
-              child:  Text('My books',textDirection:TextDirection.ltr ,style: TextStyle(
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                color: Colors.white,
-              ),),
-            ),margin: EdgeInsets.only(left: 70),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors:[Color.fromRGBO(242,156 ,91 , 50),Color.fromRGBO(248, 106,99 , 50)],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
-            ),
-            height: 185,width: 500,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: FlatButton(onPressed: (){},
-              child:  Text('Search',textDirection:TextDirection.ltr ,style: TextStyle(
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                color: Colors.white,
-              ),),
-            ), margin: EdgeInsets.only(left: 70),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors:[Color.fromRGBO(248,212 ,211 , 50),Color.fromRGBO(241, 113, 112, 50)],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
-            ),
-            height: 185,width: 500,
-          ),
-        ],
-      ),
+        ),
     );
   }
 }
